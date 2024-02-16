85c16d611b94ea1970542606c3d152278b2ed9c3
untitled-beaver <github@encamy.com>
2024-02-16 10:21:52 +0300

    Properly set up project

--- src/TaskProcessor/Program.cs
+++ src/TaskProcessor/Program.cs
@@ -1,11 +1,47 @@
 
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Web;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
 namespace TaskProcessor
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static readonly Logger m_logger = LogManager.GetCurrentClassLogger();
+
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
+            AssemblyName assemblyName = Assembly.GetEntryAssembly().GetName();
+            m_logger.Info($"Starting {assemblyName.Name} {assemblyName.Version}");
+
+            try
+            {
+                await MainWrapped(args);
+            }
+            catch (Exception ex)
+            {
+                m_logger.Fatal(ex, "Unhandled exception!");
+            }
+
+            m_logger.Info("Shutting down");
+        }
+
+        public static async Task MainWrapped(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Logging.ClearProviders();
+            builder.Logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+            builder.Host.UseNLog();
 
             // Add services to the container.
 
@@ -14,7 +50,7 @@ namespace TaskProcessor
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-            var app = builder.Build();
+            WebApplication app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -25,10 +61,32 @@ namespace TaskProcessor
 
             app.UseAuthorization();
 
-
             app.MapControllers();
 
-            app.Run();
+            await app.RunAsync();
+        }
+
+        private static void TaskScheduler_UnobservedTaskException(object? sender, UnobservedTaskExceptionEventArgs e)
+        {
+            m_logger.Fatal(e.Exception, "Got UnobservedTaskException. Shutting down!");
+
+            LogManager.Flush();
+            Environment.Exit(1);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception exception)
+            {
+                m_logger.Fatal(exception, "Got UnhandledException. Shutting down!");
+            }
+            else
+            {
+                m_logger.Fatal($"Got UnhandledException. Shutting down!");
+            }
+
+            LogManager.Flush();
+            Environment.Exit(1);
         }
     }
 }
